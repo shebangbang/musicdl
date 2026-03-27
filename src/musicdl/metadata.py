a9e1b7268c0d86d7c603a4aca13f74fd4ade2ba7
arlecchino <shravansridhar@outlook.com>
2026-03-27 16:48:08 +0530

    feat: implement tagging for mp3 files based on info in Track object

--- src/musicdl/metadata.py
+++ src/musicdl/metadata.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from mutagen.flac import FLAC, Picture
+from mutagen.id3 import APIC, ID3, TALB, TBPM, TCOP, TDAT, TIT2, TPE1, TPE2, TPOS, TRCK, TSRC, TXXX, ID3NoHeaderError
 
 from musicdl.models import Track
 
@@ -49,7 +50,7 @@ def _format_replay_gain(type: str, value: float) -> str:
         return f"{value:.6f}"
 
 
-def _embed_cover(cover_location: Path) -> Picture:
+def _embed_cover_flac(cover_location: Path) -> Picture:
     with cover_location.open("rb") as cover:
         picture = Picture()
         picture.type = 3
@@ -58,10 +59,23 @@ def _embed_cover(cover_location: Path) -> Picture:
         return picture
 
 
+def _embed_cover_mp3(cover_location: Path) -> APIC:
+    with cover_location.open("rb") as cover:
+        picture = APIC(
+            encoding=3,
+            mime="image/jpeg",
+            type=3,
+            desc="Cover",
+            data=cover.read(),
+        )
+        return picture
+
+
 def write_flac_metadata(track_location: Path, cover_location: Path, track_info: Track):
     """
     Access Track dataclass to tag the corresponding fields in the flac file.
     """
+
     track = FLAC(track_location)
     track.clear()
     track.clear_pictures()
@@ -82,14 +96,53 @@ def write_flac_metadata(track_location: Path, cover_location: Path, track_info: 
         else:
             track[tag] = str(value)
 
-    track.add_picture(_embed_cover(cover_location))
+    track.add_picture(_embed_cover_flac(cover_location))
     track.save()
 
 
 def write_mp3_metadata(track_location: Path, cover_location: Path, track_info: Track):
     """
-    Access Track dataclass to tag the corresponding fields in the flac file.
+    Access Track dataclass to tag the corresponding fields in the mp3 file.
     """
 
-    # TODO: Implement mp3 tagging
-    pass
+    try:
+        tags = ID3(track_location)
+        tags.clear()
+    except ID3NoHeaderError:
+        tags = ID3()
+
+    for attribute, tag in MP3_TAG_MAP.items():
+        value = getattr(track_info, attribute)
+        if not value:
+            continue
+
+        if tag == "TIT2":
+            tags.add(TIT2(encoding=3, text=str(value)))
+        elif tag == "TALB":
+            tags.add(TALB(encoding=3, text=str(value)))
+        elif tag == "TPE1":
+            tags.add(TPE1(encoding=3, text=value))
+        elif tag == "TPE2":
+            tags.add(TPE2(encoding=3, text=str(value)))
+        elif tag == "TRCK":
+            tags.add(TRCK(encoding=3, text=str(value)))
+        elif tag == "TPOS":
+            tags.add(TPOS(encoding=3, text=str(value)))
+        elif tag == "TSRC":
+            tags.add(TSRC(encoding=3, text=str(value)))
+        elif tag == "TCOP":
+            tags.add(TCOP(encoding=3, text=str(value)))
+        elif tag == "TDAT":
+            tags.add(TDAT(encoding=3, text=str(value.year)))
+        elif tag == "TBPM":
+            tags.add(TBPM(encoding=3, text=str(value)))
+        elif "REPLAYGAIN" in tag:
+            field = tag.split(":")[1]
+            if "_GAIN" in field:
+                formatted = _format_replay_gain("gain", value)
+            else:
+                formatted = _format_replay_gain("peak", value)
+            tags.add(TXXX(encoding=3, desc=field, text=formatted))
+
+    tags.add(_embed_cover_mp3(cover_location))
+    tags.save(track_location)
